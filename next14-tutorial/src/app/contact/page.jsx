4496e556a69ad1935ca4e43863721a528a0e34db
Aaron <ms.dev.week@gmail.com>
2024-02-28 06:05:29 +0900

    add contact design

--- next14-tutorial/src/app/contact/page.jsx
+++ next14-tutorial/src/app/contact/page.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import Image from 'next/image';
+import styles from './contact.module.css';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          alt="Contact Image"
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name"></input>
+          <input type="email" placeholder="Email address"></input>
+          <input type="text" placeholder="Phone Number (optional)"></input>
+          <textarea type="text" placeholder="message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
