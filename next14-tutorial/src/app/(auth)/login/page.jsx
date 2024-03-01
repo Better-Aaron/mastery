7267ba2783b9be7951117e29070f4eb3c487b780
Aaron <ms.dev.week@gmail.com>
2024-03-02 00:22:33 +0900

    lib issue solve

--- next14-tutorial/src/app/(auth)/login/page.jsx
+++ next14-tutorial/src/app/(auth)/login/page.jsx
@@ -1,7 +1,21 @@
+import { auth, signIn } from '@/lib/auth';
 import React from 'react';
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = async () => {
+  const session = await auth();
+
+  console.log(session);
+  const handleGithubLogin = async () => {
+    'use server';
+    await signIn('github');
+  };
+  return (
+    <div>
+      <form action={handleGithubLogin}>
+        <button>Login with Github</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
