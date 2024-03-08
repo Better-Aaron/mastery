f80085308021e30d860f822b3f0788afc62cb281
Aaron <ms.dev.week@gmail.com>
2024-03-08 16:37:27 +0900

    React - tailwindcss 기본설정

---
+++ tailwind2/tailwind.config.js
@@ -0,0 +1,102 @@
+/** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
+import plugin from "tailwindcss/plugin";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/assets/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        color: {
+          1: "#AC6AFF",
+          2: "#FFC876",
+          3: "#FF776F",
+          4: "#7ADB78",
+          5: "#858DFF",
+          6: "#FF98E2",
+        },
+        stroke: {
+          1: "#26242C",
+        },
+        n: {
+          1: "#FFFFFF",
+          2: "#CAC6DD",
+          3: "#ADA8C3",
+          4: "#757185",
+          5: "#3F3A52",
+          6: "#252134",
+          7: "#15131D",
+          8: "#0E0C15",
+          9: "#474060",
+          10: "#43435C",
+          11: "#1B1B2E",
+          12: "#2E2A41",
+          13: "#6C7275",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-sora)", ...fontFamily.sans],
+        code: "var(--font-code)",
+        grotesk: "var(--font-grotesk)",
+      },
+      letterSpacing: {
+        tagline: ".15em",
+      },
+      spacing: {
+        0.25: "0.0625rem",
+        7.5: "1.875rem",
+        15: "3.75rem",
+      },
+      opacity: {
+        15: ".15",
+      },
+      transitionDuration: {
+        DEFAULT: "200ms",
+      },
+      transitionTimingFunction: {
+        DEFAULT: "linear",
+      },
+      zIndex: {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+      },
+      borderWidth: {
+        DEFAULT: "0.0625rem",
+      },
+      backgroundImage: {
+        "radial-gradient": "radial-gradient(var(--tw-gradient-stops))",
+        "conic-gradient":
+          "conic-gradient(from 225deg, #FFC876, #79FFF7, #9F53FF, #FF98E2, #FFC876)",
+        "benefit-card-1": "url(assets/benefits/card-1.svg)",
+        "benefit-card-2": "url(assets/benefits/card-2.svg)",
+        "benefit-card-3": "url(assets/benefits/card-3.svg)",
+        "benefit-card-4": "url(assets/benefits/card-4.svg)",
+        "benefit-card-5": "url(assets/benefits/card-5.svg)",
+        "benefit-card-6": "url(assets/benefits/card-6.svg)",
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addBase, addComponents, addUtilities }) {
+      addBase({});
+      addComponents({
+        ".container": {
+          "@apply max-w-[77.5rem] mx-auto px-5 md:px-10 lg:px-15 xl:max-w-[87.5rem]":
+            {},
+        },
+      });
+      addUtilities({
+        ".tap-highlight-color": {
+          "-webkit-tap-highlight-color": "rgba(0, 0, 0, 0)",
+        },
+      });
+    }),
+  ],
+};
