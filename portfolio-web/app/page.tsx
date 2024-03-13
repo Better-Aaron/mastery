0a67bce5e0c88b6f8b6cc9a2e28720d2d63e40ea
Aaron <ms.dev.week@gmail.com>
2024-03-13 18:05:11 +0900

    heroSecsion

--- portfolio-web/app/page.tsx
+++ portfolio-web/app/page.tsx
@@ -1,9 +1,12 @@
+import HeroSection from '@/components/HeroSection';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      this is main page
+    <main className="flex min-h-screen flex-col bg-[#121212]  ">
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   );
 }
