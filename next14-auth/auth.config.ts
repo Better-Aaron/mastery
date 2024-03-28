ca4c50f7a64d755a1d9f6440841a97d24b29cb32
Aaron <ms.dev.week@gmail.com>
2024-03-28 21:21:47 +0900

    기본 credential login

--- next14-auth/auth.config.ts
+++ next14-auth/auth.config.ts
@@ -1,7 +1,32 @@
-import GitHub from "next-auth/providers/github";
-
+import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
+import bcrypt from "bcryptjs";
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "./data/user";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validateField = LoginSchema.safeParse(credentials);
+
+        if (validateField.success) {
+          const { email, password } = validateField.data;
+
+          const user = await getUserByEmail(email);
+          //* 소셜 가입 유저가 로그인 시도 했을때
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compareSync(
+            password,
+            user.password,
+          );
+
+          if (passwordsMatch) return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
