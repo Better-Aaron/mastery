654c4e1f43a35268b86961adb9dc569964eb28e6
Aaron <ms.dev.week@gmail.com>
2024-05-17 09:04:21 +0900

    add fiverr init

---
+++ next14-fiverr/app/convex-client-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { ReactNode } from "react";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function ConvexClientProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
---
+++ next14-fiverr/convex/users.ts
@@ -0,0 +1,30 @@
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const store = mutation({
+  args: {},
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Called storeUser without authenticated user");
+    }
+    // check if user is already stored
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_token", (q) =>
+        q.eq("tokenIdentifier", identity.tokenIdentifier)
+      )
+      .unique();
+
+    if (user !== null) {
+      return user._id;
+    }
+
+    const userId = await ctx.db.insert("users", {
+      tokenIdentifier: identity.tokenIdentifier,
+      email: identity.email!,
+    });
+
+    return userId;
+  },
+});
